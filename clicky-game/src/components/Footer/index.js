4f2e3645bdf898dfbe966072ebf19129ff1186ba
Adam Miller <miad0801@gmail.com>
2019-11-18 17:29:34 -0600

    Added footer

---
+++ clicky-game/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return(
+        <div className= "footer">
+
+            Copyright 2019 - Adam Miller
+
+        </div>
+    );
+};
+
+export default Footer;
