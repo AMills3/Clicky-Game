abe74f12e64d684f9ada5cc14981d25c9c26da40
Adam Miller <miad0801@gmail.com>
2019-11-18 16:56:42 -0600

    Adding files

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import cardsContainer from "./components/cardsContainer";
 
-function App() {
+class App extends Component {
+  state = {
+    cardsSelected: [],
+    score: 0,
+    highScore: 0,
+    message: "Begin game."
+  };
+
+//Clicking on an image
+  clickImage = event => {
+  let imageId = event.target.id;
+
+//Players loses if they click on an image twice
+  if(this.state.cardSelected.includes(imageId)) {
+  this.setState({
+    score: 0, 
+    cardSelected: [],
+    message: "This card was already chosen.",
+  });
+
+  } else {
+    this.state.cardSelected.push(imageId);
+
+//Updating the score
+if(this.state.highScore <= this.state.score) {
+  this.setState({
+    highScore: this.state.score + 1,
+    message: "Card never chosen.",
+    score: this.state.score + 1
+  })
+  } else {
+  this.setState({
+    message: "Card never chosen.",
+    score: this.state.score + 1
+      })
+    }
+  }
+}
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar message={this.state.message} score={this.state.score} highScore={this.state.highScore}/>
+      <Header />
+
+      (Need to define cardsContainer as a function later)
+      <cardsContainer clickImage={this.clickImage}/>
+      <Footer />
     </div>
   );
+}
+
 }
 
 export default App;
