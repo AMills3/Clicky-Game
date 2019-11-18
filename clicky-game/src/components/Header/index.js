7c5d8b18fc8411d4f79f1174e2f9bd13204cdbae
Adam Miller <miad0801@gmail.com>
2019-11-18 17:13:51 -0600

    Added header

---
+++ clicky-game/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+    return(
+        <div className= "header">
+
+            <h1>Click on an image below to score. Clicking on the same image twice ends your game. Good luck.</h1>
+
+        </div>
+    );
+};
+
+export default Header;
